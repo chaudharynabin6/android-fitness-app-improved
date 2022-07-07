2f0728900dcf3012547f94c86b620db73ff0f7da
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-07-07 21:33:05 +0545

    location permission handling

---
+++ app/src/main/java/com/androiddevs/runningappyt/permission/location/Constants.kt
@@ -0,0 +1,5 @@
+package com.androiddevs.runningappyt.permission.location
+
+object Constants {
+    const val REQUEST_CODE_LOCATION_PERMISSION = 1
+}
--- app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
+++ app/src/main/java/com/androiddevs/runningappyt/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.androiddevs.runningappyt.ui
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -7,10 +9,14 @@ import androidx.navigation.Navigation
 import androidx.navigation.ui.setupWithNavController
 import com.androiddevs.runningappyt.R
 import com.androiddevs.runningappyt.databinding.ActivityMainBinding
+import com.androiddevs.runningappyt.permission.location.Constants
+import com.androiddevs.runningappyt.permission.location.LocationPermission
 import dagger.hilt.android.AndroidEntryPoint
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +45,65 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
         }
+        sendEvent(MainActivityEvents.RequestLocationPermission)
     }
 
+    private fun sendEvent(events: MainActivityEvents){
+        when(events){
+            MainActivityEvents.RequestLocationPermission -> {
+                requestPermission()
+            }
+        }
+    }
+    private fun requestPermission() {
+        if (LocationPermission.hasLocationPermission(this)) {
+            return
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permission to use the app",
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permission to use the app",
+                Constants.REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+//        no operation
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)
+        ) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermission()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 }
 
 
+internal sealed class MainActivityEvents(){
+    object RequestLocationPermission : MainActivityEvents()
+}
