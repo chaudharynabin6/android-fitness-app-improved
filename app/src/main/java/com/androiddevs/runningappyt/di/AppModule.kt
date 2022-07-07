005ebc29a3241952493a25c4824afa8cc01578d6
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-07-07 12:51:18 +0545

    di : dagger hilt setup

---
+++ app/src/main/java/com/androiddevs/runningappyt/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.androiddevs.runningappyt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.androiddevs.runningappyt.db.Constants.RUNNING_DATABASE_NAME
+import com.androiddevs.runningappyt.db.RunDAO
+import com.androiddevs.runningappyt.db.RunningDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRunningDatabase(
+        @ApplicationContext app: Context
+    ): RunningDatabase = Room.databaseBuilder(
+        app,
+        RunningDatabase::class.java,
+        RUNNING_DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(
+        db: RunningDatabase
+    ): RunDAO = db.getRunDao()
+}
